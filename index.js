e14f2a087879a8bfa235cafc19e7575b5c35775b
Stephanie Coy <scoy028@gmail.com>
2018-11-04 13:46:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,49 @@
+// function sayHello() {
+//   console.log("Hello!")
+// }
+// sayHello()
+
+// function sayHelloToIsabel() {
+//   console.log("Hello, Isabel!")
+// }
+// sayHelloToIsabel()
+
+// function sayHelloToSofia() {
+//   console.log("Hello, Sofia!")
+// }
+// sayHelloToSofia()
+
+// function sayHelloToBrendan() {
+//   console.log("Hello, Brendan!")
+// }
+// sayHelloToBrendan()
+
+// function sayHelloTo(firstName) {
+//   console.log(`Hello, ${firstName}!`)
+// }
+
+// sayHelloTo("Stephanie")
+
+// function say(greeting, firstName) {
+//   console.log(`${greeting}, ${firstName}!`)
+// }
+// say("Yo", "Joe")
+
+
+function add(x, y) {
+  return x + y
+}
+// console.log(add(1, 1));
+
+
+
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+}
+console.log(say("Hello", "Sofia"));
+
+function say2() {
+  return undefined;
+}
+
+console.log('say 2:', say2());
